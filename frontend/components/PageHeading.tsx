3e4af95b5d69a685b3c3a1b39d43aaa2764ca4fd
theDudekat <45604098+theDudekat@users.noreply.github.com>
2023-01-17 17:50:11 +0200

    Test

--- frontend/components/PageHeading.tsx
+++ frontend/components/PageHeading.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function PageHeading({ children }: PropsWithChildren<{}>) {
-  return <h1 className="self-center pb-8 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-fuchsia-600 to-pink-600">{children}</h1>
+  return <h1 className="self-center pb-8 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-orange-600 to-orange-600">{children}</h1>
 }
